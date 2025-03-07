ce6d77c94fd9e443f4c6d0dbaa21e084f89f2650
Pelikh Ilya <iipelikh@yandex.ru>
2025-03-07 17:13:38 +0300

    init

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-// const { writeFileSync } = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.json());
 
 app.use(express.static('public/static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+app.post('/tasks/:id', (req, res) => {
+  console.log('body:', req.body?.code, req.params.id);
+  // логика теста типа eval('...')
+  if (true) {
+    const taskId = Number(req.params.id);
+    res.setHeader('Location', `http://localhost:3000/tasks/${taskId + 1}`);
+  }
+  res.status(301).send();
 });
 
-app.post('/policy-report/*', req => {
-  console.log(req.files);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
